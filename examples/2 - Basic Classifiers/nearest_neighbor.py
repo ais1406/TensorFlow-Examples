ac1e4e129c0f2a09357d9e382651de8e41d95b78
inseonan <ais1406@gmail.com>
2016-05-27 17:08:44 +0900

    for python 3.5

--- examples/2 - Basic Classifiers/nearest_neighbor.py
+++ examples/2 - Basic Classifiers/nearest_neighbor.py
@@ -10,8 +10,8 @@ import numpy as np
 import tensorflow as tf
 
 # Import MINST data
-import input_data as dataset
-mnist = dataset.read_data_sets("/tmp/data/", one_hot=True)
+import input_data
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
 # In this example, we limit mnist data
 Xtr, Ytr = mnist.train.next_batch(5000) #5000 for training (nn candidates)
--- examples/3 - Neural Networks/bidirectional_rnn.py
+++ examples/3 - Neural Networks/bidirectional_rnn.py
@@ -150,14 +150,14 @@ with tf.Session() as sess:
             loss = sess.run(cost, feed_dict={x: batch_xs, y: batch_ys,
                                              istate_fw: np.zeros((batch_size, 2*n_hidden)),
                                              istate_bw: np.zeros((batch_size, 2*n_hidden))})
-            print "Iter " + str(step*batch_size) + ", Minibatch Loss= " + "{:.6f}".format(loss) + \
-                  ", Training Accuracy= " + "{:.5f}".format(acc)
+            print ("Iter " + str(step*batch_size) + ", Minibatch Loss= " + "{:.6f}".format(loss) + \
+                  ", Training Accuracy= " + "{:.5f}".format(acc))
         step += 1
-    print "Optimization Finished!"
+    print ("Optimization Finished!")
     # Calculate accuracy for 128 mnist test images
     test_len = 128
     test_data = mnist.test.images[:test_len].reshape((-1, n_steps, n_input))
     test_label = mnist.test.labels[:test_len]
-    print "Testing Accuracy:", sess.run(accuracy, feed_dict={x: test_data, y: test_label,
+    print ("Testing Accuracy:", sess.run(accuracy, feed_dict={x: test_data, y: test_label,
                                                              istate_fw: np.zeros((test_len, 2*n_hidden)),
-                                                             istate_bw: np.zeros((test_len, 2*n_hidden))})
+                                                             istate_bw: np.zeros((test_len, 2*n_hidden))}))
